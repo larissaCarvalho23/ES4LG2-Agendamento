32f65065f36591c7abfd79aabba88771b593669a
ramonsouza27 <ramonsouzadeoliveira94@gmail.com>
2019-12-06 08:33:37 -0300

    Editar subcliente parcial

--- Projeto/src/main/java/br/edu/ifsp/lp2a4/hellospring/ClienteController.java
+++ Projeto/src/main/java/br/edu/ifsp/lp2a4/hellospring/ClienteController.java
@@ -1,6 +1,7 @@
 package br.edu.ifsp.lp2a4.hellospring;
 
 import java.util.Date;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -85,6 +86,70 @@ public class ClienteController {
 	public String remove(@PathVariable long id, Model model) {
 		
 		repository.deleteById(id);
+		
+		model.addAttribute("cliente", repository.findAll()); 
+		
+		return "clientes/list";
+	}
+	
+	@GetMapping("/clientes/{id}/edit")
+	public String edit (@PathVariable long id,Model model) {
+		
+		SubCliente subcliente = repository
+				.findById(id)
+				.orElseThrow(()->new ResourceNotFoundException("Cliente não encontrado"));
+		
+		System.out.println("************get**************");
+		
+		SubClienteView cli = new SubClienteView(); 
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");		
+
+		String date = dateFormat.format(subcliente.getDataNasc());
+		
+		cli.setId(subcliente.getId());
+		cli.setNome(subcliente.getNome());
+		cli.setSexo(subcliente.getSexo());
+		cli.setCep(subcliente.getCep());
+		cli.setCidade(subcliente.getCidade());
+		cli.setDataNasc(date);
+		cli.setEmail(subcliente.getEmail());
+		cli.setLogradouro(subcliente.getLogradouro());
+		cli.setNumeroend(subcliente.getNumeroend());
+		cli.setTelefone(subcliente.getTelefone());
+		cli.setUf(subcliente.getUf());
+		
+		model.addAttribute("cliente",cli);
+		
+		return "clientes/edit";
+	}
+	
+	@PostMapping("/clientes/{id}")
+	public String edit(@PathVariable long id, @Valid SubClienteView cliente, BindingResult result, Model model)  throws ParseException {
+
+		
+		System.out.println("************post**************");
+		if(result.hasErrors())
+			return "clientes/edit";
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = formatter.parse(cliente.getDataNasc());
+		
+		SubCliente cli = new SubCliente();
+		
+		cli.setNome(cliente.getNome());
+		cli.setSexo(cliente.getSexo());
+		cli.setCep(cliente.getCep());
+		cli.setCidade(cliente.getCidade());
+		cli.setDataNasc(date);
+		cli.setEmail(cliente.getEmail());
+		cli.setLogradouro(cliente.getLogradouro());
+		cli.setNumeroend(cliente.getNumeroend());
+		cli.setTelefone(cliente.getTelefone());
+		cli.setUf(cliente.getUf());
+		
+		repository.save(cli);
 		
 		model.addAttribute("cliente", repository.findAll()); 
 		
